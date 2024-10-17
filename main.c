0ab563535d5533b06e81028b116044a6408bba79
klauty <kaue.developer@gmail.com>
2024-10-16 22:31:27 -0300

    criando as áreas principais da tela de desenho e ferramentas

--- main.c
+++ main.c
@@ -4,8 +4,13 @@
 #include <SDL3/SDL_main.h>
 
 /* We will use this renderer to draw into this window every frame. */
-static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static SDL_Window *window = NULL;
+SDL_FRect mouseCursor;
+float mouseX = 0;
+float mouseY = 0;
+SDL_FPoint mousePoint;
+
 
 
 /* This function runs once at startup. */
@@ -20,6 +25,10 @@ int SDL_AppInit(void **appstate, int argc, char *argv[])
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't create window/renderer!", SDL_GetError(), NULL);
         return SDL_APP_FAILURE;
     }
+    
+    mouseCursor.w = mouseCursor.h = 20;
+    
+
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
@@ -29,6 +38,12 @@ int SDL_AppEvent(void *appstate, const SDL_Event *event)
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
+
+    if(event->type == SDL_EVENT_MOUSE_MOTION){
+        SDL_GetMouseState(&mousePoint.x,&mousePoint.y);
+
+        //SDL_Log("x %f,y %f ",mouseX, mouseY);
+    }
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
@@ -36,16 +51,35 @@ int SDL_AppEvent(void *appstate, const SDL_Event *event)
 int SDL_AppIterate(void *appstate)
 {
 	SDL_FRect rect;
-	/* as you can see from this, rendering draws over whatever was drawn before it. */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  /* black, full alpha */
-    SDL_RenderClear(renderer);  /* start with a blank canvas. */
+    SDL_FRect rectToolBar;
 
-    /* draw a filled rectangle in the middle of the canvas. */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);  /* blue, full alpha */
-    rect.x = rect.y = 100;
-    rect.w = 440;
+    rectToolBar.x = 0;
+    rectToolBar.y = 0;
+    rectToolBar.h = 300;
+    rectToolBar.w = 50;
+	
+	/* cor de fundo */
+    SDL_SetRenderDrawColor(renderer, 104, 109, 118, 255);
+    SDL_RenderClear(renderer);
+
+    // desenha fundo barra de ferramentas
+    SDL_SetRenderDrawColor(renderer, 55, 58, 64, 255);
+    SDL_RenderFillRect(renderer,&rectToolBar);
+
+    // desenha o canvas para o pixel-art, a area de desenho vai ter 32x32 para testes  
+    SDL_SetRenderDrawColor(renderer, 238, 238, 238, 255);
+    rect.x = 200; 
+    rect.y = 100;
+    rect.w = 280;
     rect.h = 280;
     SDL_RenderFillRect(renderer, &rect);
+
+    //desenha o cursor do mouse
+    mouseCursor.x = mousePoint.x;
+    mouseCursor.y = mousePoint.y;
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer,&mouseCursor);
+
     SDL_RenderPresent(renderer);  /* put it all on the screen! */
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
